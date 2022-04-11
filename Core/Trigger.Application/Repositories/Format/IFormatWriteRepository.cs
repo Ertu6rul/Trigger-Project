840e03ac618d7eda9fa38ea377392e6417874e16
BaduA <batualpustaguel@gmail.com>
2022-04-11 13:57:45 +0300

    Repos

---
+++ Core/Trigger.Application/Repositories/Format/IFormatWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trigger.Domain.Entities;
+
+namespace Trigger.Application.Repositories
+{
+    public interface IFormatWriteRepository : IWriteRepository<Format>
+    {
+    }
+}
---
+++ Core/Trigger.Domain/Entities/Format.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trigger.Domain.Entities.Common;
+
+namespace Trigger.Domain.Entities
+{
+    public class Format: BaseEntity
+    {
+        public string Voice1Url { get; set; }
+        public string Voice2Url { get; set; }
+        public string Voice3Url { get; set; }
+        public string Voice4Url { get; set; }
+        public string Voice5Url { get; set; }
+        public string Voice6Url { get; set; }
+        public int Score { get; set; }
+    }
+}
